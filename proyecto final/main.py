7b44e264864e897b03625554925eae022112ee55
josemariocarter <josemariocarter@outlook.com>
2016-04-15 19:46:49 -0500

    integrantes

--- proyecto final/main.py
+++ proyecto final/main.py
@@ -1,3 +1,14 @@
+"""
+Alexander Cerrud 8-850-2381
+Luís Wong 8-894-456
+Pedro perez se 1788657
+Humberto Caceres 8-875-1617
+Miguel Tejada 8-868-1026
+Jose mario carter 8-915-1458
+Bonilla jose luis 8-886-2453
+Mario Martínez 062905639
+David Acevedo 8924137
+Boris montalvo 8-848-1016"""""
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
